63046a967ca54e3dd0bdb42e74bec5b316d0ef5b
nraudseps <nraudseps@gmail.com>
2020-11-20 12:28:27 +0200

    base project completed

---
+++ app/Models/User.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable, Followable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'username',
+        'avatar',
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+    /**
+     * @var mixed
+     */
+
+    public function getAvatarAttribute($value)
+    {
+        return asset($value ? 'storage/' . $value : 'images/default-avatar-2.jpeg');
+    }
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
+
+    public function timeline()
+    {
+        $friends = $this->follows()->pluck('id');
+
+        return Tweet::withLikes()
+            ->whereIn('user_id', $friends)
+            ->orWhere('user_id', $this->id)
+            ->latest()
+            ->get();
+    }
+
+    public function tweets()
+    {
+        return $this->hasMany(Tweet::class);
+    }
+
+    public function path($append = '')
+    {
+        $path =  route('profile', $this->username);
+
+        return $append ? "{$path}/{$append}" : $path;
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+}
---
+++ resources/views/components/follow-button.blade.php
@@ -0,0 +1,12 @@
+@if(auth()->user()->isNot($user))
+    <form action="{{ route('follow', $user->username) }}" method="post">
+        @csrf
+
+        <button
+            type="submit"
+            class="bg-blue-500 rounded-full shadow py-2 px-4 text-white text-xs"
+        >
+            {{ auth()->user()->following($user) ? 'Unfollow Me' : 'Follow Me' }}
+        </button>
+    </form>
+@endif
